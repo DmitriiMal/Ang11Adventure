531caf999ec91ca2d8a1283e60ff984f698a18cd
Dmitrii Malyshkin <d.w.malyschkin@gmail.com>
2023-10-16 22:13:39 +0200

    ivents, conditionals, loop for of

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  clickCounter: number = 0;
+  students: Array<{ name: string; age: number }> = [
+    {
+      name: 'David',
 
+      age: 32,
+    },
+    {
+      name: 'jime',
+
+      age: 28,
+    },
+    {
+      name: 'sabrina',
+
+      age: 26,
+    },
+  ];
+
+  constructor() {}
+
+  countClick() {
+    this.clickCounter++;
+  }
+
+  ngOnInit(): void {}
 }
